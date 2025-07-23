bd1b2c2b44291d46a05ee82c7e181acc8bb2f657
SeyyidOS <seyyidosmansevgili@gmail.com>
2025-07-24 01:12:21 +0300

    Added hostname

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  hostname: "0.0.0.0",
+  port: 5173,
   base: "/FifaMatchups/",
   plugins: [react()],
 });
